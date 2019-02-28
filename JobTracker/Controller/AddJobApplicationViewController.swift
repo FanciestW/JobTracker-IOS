53680076e82c232a152f6f05b4bde3c75d02ac75
William Lin <wlin26@yahoo.com>
2019-02-27 23:07:39 -0500

    Updated git ignore file

---
+++ JobTracker/Controller/AddJobApplicationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddJobApplicationViewController.swift
+//  JobTracker
+//
+//  Created by William Lin on 2/27/19.
+//  Copyright © 2019 William Lin. All rights reserved.
+//
+
+import UIKit
+
+class AddJobApplicationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
