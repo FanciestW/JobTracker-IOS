2a46b813b8c6897635f328470cb55bc6154302a5
William Lin <wlin26@yahoo.com>
2019-03-01 00:08:10 -0500

    Updated travis file

---
+++ JobTracker/Controller/AppliedListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AppliedListViewController.swift
+//  JobTracker
+//
+//  Created by William Lin on 2/28/19.
+//  Copyright © 2019 William Lin. All rights reserved.
+//
+
+import UIKit
+
+class AppliedListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
