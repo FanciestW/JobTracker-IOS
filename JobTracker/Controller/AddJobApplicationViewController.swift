1b58a11177256decb39b825c1fbe9ab1c96b96fe
William Lin <wlin26@yahoo.com>
2019-03-10 20:02:15 -0400

    Added UserDefaults saving of field values
    When a user is adding a new job application, they don't have to worry about losing their data. It will persist from one screen to another and also after the app have been closed. This all done through UserDefaults since it is only saving text data.

--- JobTracker/Controller/AddJobApplicationViewController.swift
+++ JobTracker/Controller/AddJobApplicationViewController.swift
@@ -21,7 +21,12 @@ class AddJobApplicationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
-        // Do any additional setup after loading the view.
+        loadJobApplicationValues()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        saveJobApplicationValues()
     }
     
     @IBAction func btnClearFieldsClicked(_ sender: Any) {
@@ -31,6 +36,54 @@ class AddJobApplicationViewController: UIViewController {
         jobTypeTextField.text = ""
         jobLocationTextField.text = ""
         jobNoteTextView.text = ""
+    }
+    
+    func loadJobApplicationValues() {
+        // Load existing values
+        if let saveJobTitle = UserDefaults.standard.value(forKey: "saveJobTitle") {
+            jobTitleTextField.text = saveJobTitle as? String ?? ""
+        } else {
+            UserDefaults.standard.set(jobTitleTextField.text, forKey: "saveJobTitle")
+        }
+        
+        if let saveCompany = UserDefaults.standard.value(forKey: "saveJobCompany") {
+            companyTextField.text = saveCompany as? String ?? ""
+        } else {
+            UserDefaults.standard.set(companyTextField.text, forKey: "saveJobCompany")
+        }
+        
+        if let saveAppliedDate = UserDefaults.standard.value(forKey: "saveJobAppliedDate") {
+            appliedDateTextField.text = saveAppliedDate as? String ?? ""
+        } else {
+            UserDefaults.standard.set(appliedDateTextField.text, forKey: "saveJobAppliedDate")
+        }
+        
+        if let saveJobType = UserDefaults.standard.value(forKey: "saveJobType") {
+            jobTypeTextField.text = saveJobType as? String ?? ""
+        } else {
+            UserDefaults.standard.set(jobTypeTextField.text, forKey: "saveJobType")
+        }
+        
+        if let saveJobLocation = UserDefaults.standard.value(forKey: "saveJobLocation") {
+            jobLocationTextField.text = saveJobLocation as? String ?? ""
+        } else {
+            UserDefaults.standard.set(jobLocationTextField.text, forKey: "saveJobLocation")
+        }
+        
+        if let saveJobNote = UserDefaults.standard.value(forKey: "saveJobNote") {
+            jobNoteTextView.text = saveJobNote as? String ?? ""
+        } else {
+            UserDefaults.standard.set(jobNoteTextView.text, forKey: "saveJobNote")
+        }
+    }
+    
+    func saveJobApplicationValues() {
+        UserDefaults.standard.set(jobTitleTextField.text, forKey: "saveJobTitle")
+        UserDefaults.standard.set(companyTextField.text, forKey: "saveJobCompany")
+        UserDefaults.standard.set(appliedDateTextField.text, forKey: "saveJobAppliedDate")
+        UserDefaults.standard.set(jobTypeTextField.text, forKey: "saveJobType")
+        UserDefaults.standard.set(jobLocationTextField.text, forKey: "saveJobLocation")
+        UserDefaults.standard.set(jobNoteTextView.text, forKey: "saveJobNote")
     }
     
     
