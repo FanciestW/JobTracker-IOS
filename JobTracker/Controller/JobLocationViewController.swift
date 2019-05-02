66bb562816fd9052488e06ce5de7f88a5d037638
William Lin <wlin26@yahoo.com>
2019-05-02 00:02:10 -0400

    Added location of job listings

--- JobTracker/Controller/JobLocationViewController.swift
+++ JobTracker/Controller/JobLocationViewController.swift
@@ -29,10 +29,28 @@ class JobLocationViewController: UIViewController, CLLocationManagerDelegate {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        let geocoder = CLGeocoder()
+        if geocoder.isGeocoding { geocoder.cancelGeocode() }
+        geocoder.geocodeAddressString(jobLocation, completionHandler: { placemarks, error in
+            if (error != nil) {
+                return
+            }
+            let location = placemarks![0].location
+            let region = MKCoordinateRegion(center: location!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+            let mapAnnotation = MKPointAnnotation()
+            mapAnnotation.coordinate = location!.coordinate
+            self.mapView.setCenter(location!.coordinate, animated: true)
+            self.mapView.setRegion(region, animated: true)
+            self.mapView.addAnnotation(mapAnnotation)
+        })
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let region = MKCoordinateRegion(center: (locations.last?.coordinate)!, latitudinalMeters: 200, longitudinalMeters: 200)
         mapView.setRegion(region, animated: true)
-        mapView.showsUserLocation = true
         
     }
     
